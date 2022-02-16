7574ecb27c186918c08f04dcc0ee0e996b7fa1eb
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-16 12:42:07 -0500

    working on routers and homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-
+import {
+  Exchanges,
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from "./components";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="app">
-        <div className="navbar"></div>
+        <div className="navbar">
+          <Navbar />
+        </div>
 
-        <div className="main"></div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route exact path="/" element={<Homepage />} />
+              </Routes>
+              <Routes>
+                <Route exact path="/exchanges" element={<Exchanges />} />
+              </Routes>
+              <Routes>
+                <Route
+                  exact
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                />
+              </Routes>
+              <Routes>
+                <Route
+                  exact
+                  path="/crypto/:coinId"
+                  element={<CryptoDetails />}
+                />
+              </Routes>
+              <Routes>
+                <Route exact path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
 
-        <div className="footer"></div>
+          <div className="footer">
+            <Typography.Title
+              level={5}
+              style={{ color: "white", textAlign: "center" }}
+            >
+              Copyright © 2021
+              <Link to="/">Cryptoverse Inc.</Link> <br />
+              All Rights Reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
+        </div>
       </div>
-      {/* <Router>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-        </Routes>
-      </Router> */}
     </>
   );
 }
 
 export default App;
+
+//  <Router>
+//   <Routes>
+//     <Route exact path="/" element={<Landing />} />
+//   </Routes>
+// </Router>
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+
+const Homepage = () => {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
