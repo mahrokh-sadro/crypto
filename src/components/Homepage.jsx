7cad5870b150f15cc18df31d0ee66a2a946c05c3
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-16 16:10:35 -0500

    components

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -20,18 +20,18 @@ console.log(data)
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title>
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats?.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={(globalStats?.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${(globalStats?.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${(globalStats?.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats?.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={(globalStats?.totalMarkets)} /></Col>
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
         <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
       </div>
-      {/* <Cryptocurrencies simplified /> */}
+      <Cryptocurrencies simplified />
       <div className="home-heading-container">
         <Title level={2} className="home-title">Latest Crypto News</Title>
         <Title level={3}><Link to="/news">Show more</Link></Title>
