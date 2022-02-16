213f49a489b40e002563ea15a1d04548e9cf39c6
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-16 15:45:29 -0500

    components

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+// import Loader from './Loader';
 
 const Cryptocurrencies = () => {
+
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  console.log(cryptos)
+
+
   return (
-    <div>Cryptocurrencies</div>
+
+    
+    <div>   
+       <Row gutter={[32, 32]} className="crypto-card-container">
+{cryptos.map(currency=>(
+
+  <Col  xs={24}
+  sm={12}
+  lg={6}
+  className="crypto-card"
+  key={currency.uuid}
+  >
+  <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+  <Card>
+    
+  </Card>
+  </Link>
+
+
+
+  </Col>
+))}
+       </Row>
+    </div>
   )
 }
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,27 +1,67 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const cryptoApiHeaders = {
+//   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//   "x-rapidapi-key": "146f94f0famsh0815bb32ef34550p10f457jsnbd266cc3a7ab",
+// };
+// const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+// const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+// export const cryptoApi = createApi({
+//   reducerPath: "cryptoApi",
+//   baseQuery: fetchBaseQuery({ baseUrl }),
+//   endpoints: (builder) => ({
+//     getCryptos: builder.query({
+//       query: () => createRequest("/coins"),
+//     }),
+//   }),
+// });
+
+// export const {
+//   useGetCryptosQuery,
+//   useGetCryptoDetailsQuery,
+//   useGetExchangesQuery,
+//   useGetCryptoHistoryQuery,
+// } = cryptoApi;
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Note: Change v1 to v2 on rapid api
 
 const cryptoApiHeaders = {
   "x-rapidapi-host": "coinranking1.p.rapidapi.com",
   "x-rapidapi-key": "146f94f0famsh0815bb32ef34550p10f457jsnbd266cc3a7ab",
 };
-const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
 
-export const createApi
-// var options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/coins",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     timePeriod: "24h",
-//     tiers: "1",
-//     orderBy: "marketCap",
-//     orderDirection: "desc",
-//     limit: "50",
-//     offset: "0",
-//   },
-//   headers: {
-//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-//     "x-rapidapi-key": "146f94f0famsh0815bb32ef34550p10f457jsnbd266cc3a7ab",
+    // getCryptoDetails: builder.query({
+    //   query: (coinId) => createRequest(`/coin/${coinId}`),
+    // }),
 
-// },
-// };
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    // getCryptoHistory: builder.query({
+    //   query: ({ coinId, timeperiod }) =>
+    //     createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    // }),
+
+    // Note: To access this endpoint you need premium plan
+    // getExchanges: builder.query({
+    //   query: () => createRequest("/exchanges"),
+    // }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  //   useGetCryptoDetailsQuery,
+  //   useGetExchangesQuery,
+  //   useGetCryptoHistoryQuery,
+} = cryptoApi;
